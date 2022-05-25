15890b2f2561df3f46e2573ee015364dcf957875
yuwinzer <yu.moskalev@gmail.com>
2022-05-25 17:16:35 +0300

    added app.py for work with gunicorn

---
+++ framework/api.py
@@ -0,0 +1,8 @@
+
+
+class API:
+    def __call__(self, environ, start_response):
+        response_body = b'Hello, World!'
+        status = '200 OK'
+        start_response(status, headers=[])
+        return iter([response_body])
---
+++ framework/app.py
@@ -0,0 +1,9 @@
+from api import API
+
+app = API()
+
+# def app(environ, start_response):
+#     response_body = b'Hello, World!'
+#     status = '200 OK'
+#     start_response(status, headers=[])
+#     return iter([response_body])
