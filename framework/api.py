13ff85fd7236669ee472a8dc8ad13733c0b0c76c
yuwinzer <yu.moskalev@gmail.com>
2022-05-27 21:48:30 +0300

    class based handlers added

--- framework/api.py
+++ framework/api.py
@@ -1,6 +1,6 @@
 from parse import parse
 from webob import Request, Response
-
+import inspect
 
 class API:
     def __init__(self):
@@ -32,6 +32,10 @@ class API:
         handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
