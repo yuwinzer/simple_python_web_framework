8bb964a29823f8855a4da107cc5a64a84f9b186f
yuwinzer <yu.moskalev@gmail.com>
2022-05-27 09:17:13 +0300

    static routes

--- framework/api.py
+++ framework/api.py
@@ -1,8 +1,47 @@
+from webob import Request, Response
 
 
 class API:
+    def __init__(self):
+        self.routes = {}
+
     def __call__(self, environ, start_response):
-        response_body = b'Hello, World!'
-        status = '200 OK'
-        start_response(status, headers=[])
-        return iter([response_body])
+        request = Request(environ)
+
+        response = self.handle_request(request)
+
+        return response(environ, start_response)
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found"
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            return handler
+
+        return wrapper
+
+    def handle_request(self, request):
+        response = Response()
+
+        handler = self.find_handler(request_path=request.path)
+
+        if handler is not None:
+            handler(request, response)
+        else:
+            self.default_response(response)
+
+        return response
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            if path == request_path:
+                return handler
+
+    # def __call__(self, environ, start_response):
+    #     response_body = b'Hello, World!'
+    #     status = '200 OK'
+    #     start_response(status, headers=[])
+    #     return iter([response_body])
--- framework/app.py
+++ framework/app.py
@@ -2,6 +2,16 @@ from api import API
 
 app = API()
 
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from Home page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from About page"
+
 # def app(environ, start_response):
 #     response_body = b'Hello, World!'
 #     status = '200 OK'
