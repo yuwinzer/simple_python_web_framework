6a3ed1df536c15bbe3bc29f438fc79da88a62c61
yuwinzer <yu.moskalev@gmail.com>
2022-06-08 21:33:04 +0300

    Database and Table Creation added

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from api import API
+from winz.api import API
 
 
 @pytest.fixture
@@ -11,3 +11,47 @@ def api():
 @pytest.fixture
 def client(api):
     return api.test_session()
+
+import os
+import pytest
+
+from winz.api import API
+from winz.orm import Database, Table, Column, ForeignKey
+
+
+@pytest.fixture
+def api():
+    return API()
+
+
+@pytest.fixture
+def client(api):
+    return api.test_session()
+
+
+@pytest.fixture
+def db():
+    DB_PATH = "./test.db"
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    db = Database(DB_PATH)
+    return db
+
+
+@pytest.fixture
+def Author():
+    class Author(Table):
+        name = Column(str)
+        age = Column(int)
+
+    return Author
+
+
+@pytest.fixture
+def Book(Author):
+    class Book(Table):
+        title = Column(str)
+        published = Column(bool)
+        author = ForeignKey(Author)
+
+    return Book
